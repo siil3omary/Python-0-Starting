e0081ba9c641ede211bfa123887e615614ca96fb
L3OMARY IMAC <you@example.com>
2024-09-26 00:09:15 +0100

    Refactor ft_filter function to use list comprehension for improved readability and efficiency

--- ex06/ft_filter.py
+++ ex06/ft_filter.py
@@ -1,3 +1,23 @@
+# def ft_filter1(function, iter):
+#     """
+#     ft_filter(function, iter) -> list
+#     purpose: filter elements of an iterable
+#     function: function to filter the elements
+#     iter: iterable to filter
+#     return: list of elements that passed the filter
+#     """
+#     returned_list = []
+#     if function is None:
+#         for i in iter:
+#             if i:
+#                 returned_list.append(i)
+#     else:
+#         for i in iter:
+#             if function(i):
+#                 returned_list.append(i)
+#     return returned_list
+
+
 def ft_filter(function, iter):
     """
     ft_filter(function, iter) -> list
@@ -6,13 +26,4 @@ def ft_filter(function, iter):
     iter: iterable to filter
     return: list of elements that passed the filter
     """
-    returned_list = []
-    if function is None:
-        for i in iter:
-            if i:
-                returned_list.append(i)
-    else:
-        for i in iter:
-            if function(i):
-                returned_list.append(i)
-    return returned_list
+    return [item for item in iter if (function(item) if function else item)]
