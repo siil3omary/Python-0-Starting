47186292299a596edfc3a5e7daac07987366d8e6
L3OMARY IMAC <you@example.com>
2024-09-25 23:41:28 +0100

    feat: Add ft_filter function for filtering elements in an iterable
    The code changes introduce a new function `ft_filter` that filters elements in an iterable based on a given function. The function accepts two parameters: `function` (the filtering function) and `iter` (the iterable to filter). It returns a list of elements that passed the filter.
    
    This commit message follows the established convention of using a prefix to indicate the type of change (`feat` for a new feature).

---
+++ ex06/ft_filter.py
@@ -0,0 +1,18 @@
+def ft_filter(function, iter):
+    """
+    ft_filter(function, iter) -> list
+    purpose: filter elements of an iterable
+    function: function to filter the elements
+    iter: iterable to filter
+    return: list of elements that passed the filter
+    """
+    returned_list = []
+    if function is None:
+        for i in iter:
+            if i:
+                returned_list.append(i)
+    else:
+        for i in iter:
+            if function(i):
+                returned_list.append(i)
+    return returned_list
